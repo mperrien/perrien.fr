75d495fe3c67b7b555b296528c25010696ed48fa
Marc Perrien <marc@bluelimes.dev>
2025-01-17 17:45:35 +0100

    Robots.txt (#5)
    * Don't index /contact/sent/
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Sitemap
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Configure sitemap
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Update OpenGraph image
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    ---------
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://perrien.fr",
   integrations: [
     icon({
       iconDir: "src/assets/icons",
+    }),
+    sitemap({
+      filter: (page) => page !== "https://perrien.fr/contact/sent/",
     }),
   ],
 });

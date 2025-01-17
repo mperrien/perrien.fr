4c3ec58d6d9c685af925e001ed03f8b03e811c9e
Marc Perrien <marc@bluelimes.dev>
2025-01-17 16:21:45 +0100

    Home (#2)
    * Header
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Close details on click outside
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Add two articles
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Better handle videos in cards
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * WIP. Reliably Assistant
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Assistant page content
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Clean up
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Animate
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Add charts page
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Styles
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Styles
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Legal and contact pages
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Home:header responsive
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Home:Articles responsive
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Reliably pages: responsive
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Widgets page: responsive
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Blue Limes page: responsive
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Fix menu
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Contact pages: responsive
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Legal notice: responsive
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Favicon
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Clean up README
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    ---------
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://perrien.fr",
+  integrations: [
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+  ],
 });
